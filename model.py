a246b4311966abe939023f8144930a47f2659c95
HIPERCUBE <joowonryoo@gmail.com>
2020-07-02 17:06:39 +0900

    Add simple cnn model

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, Dropout
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, Dropout, MaxPool2D
 
 
 def simple_model():
@@ -25,12 +25,25 @@ def simple_cnn():
     Score:
     """
     model = Sequential([
-
+        Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
+        MaxPool2D((2, 2)),
+        Conv2D(128, kernel_size=(3, 3), activation='relu'),
+        MaxPool2D((2, 2)),
+        Conv2D(64, kernel_size=(3, 3), activation='relu'),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dropout(0.2),
+        Dense(10, activation='softmax')
     ])
-    model.compile()
+    model.compile(
+        optimizer='adam',
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+    return model
 
 
 def build_model():
-    model = simple_model()
+    model = simple_cnn()
     model.summary()
     return model
